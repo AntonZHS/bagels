dcf469479c9c13886cc751345c5a853d82662390
AntonZHS <ant912377@gmail.com>
2022-08-21 15:39:35 +0300

    добавил функцию возврата ответов

--- bagels.py
+++ bagels.py
@@ -3,11 +3,28 @@ import random
 NUM_DIGITS = 3
 MAX_GUESS = 10
 
+
 def getSecretNum():
     numbers = list(range(10))
     random.shuffle(numbers)
     secretsNum = ''
     for i in range(NUM_DIGITS):
         secretsNum += str(numbers[i])
-    return secretsNum   
-     
+    return secretsNum
+
+
+def getClues(guess, secretNum):
+    if guess == secretNum:
+        return 'Вы угадали!'
+
+    clues = []
+    for i in range(len(guess)):
+        if guess == secretNum:
+            clues.append('Горячо')
+        elif guess[i] in secretNum:
+            clues.append('Тепло')
+    if len(clues) == 0:
+        return 'Холодно'
+
+    clues.sort()
+    return ''.join(clues)
